be00f9738442471f42d67ae32331898471261b7e
bryanlds <bryanlds5@gmail.com>
2019-12-27 20:20:18 -0300

    finish search form

--- src/apps/listings/views.py
+++ src/apps/listings/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
 from .models import Listing
+from .choices import bedroom_choices, price_choices, state_choices
 
 
 def index(request):
@@ -23,4 +24,43 @@ def listing(request, listing_id):
 
 
 def search(request):
-    return render(request, "listings/search.html")
+    query_list = Listing.objects.order_by("-list_date")
+
+    # Keywords
+    if "keywords" in request.GET:
+        keywords = request.GET["keywords"]
+        if keywords:
+            query_list = query_list.filter(description__icontains=keywords)
+
+    # City
+    if "city" in request.GET:
+        city = request.GET["city"]
+        if city:
+            query_list = query_list.filter(city__iexact=city)
+
+    # State
+    if "state" in request.GET:
+        state = request.GET["state"]
+        if state:
+            query_list = query_list.filter(state__iexact=state)
+
+    # Bedrooms
+    if "bedrooms" in request.GET:
+        bedrooms = request.GET["bedrooms"]
+        if bedrooms:
+            query_list = query_list.filter(bedrooms__lte=bedrooms)
+
+    # Price
+    if "price" in request.GET:
+        price = request.GET["price"]
+        if price:
+            query_list = query_list.filter(price__lte=price)
+
+    context = {
+        "bedroom_choices": bedroom_choices,
+        "price_choices": price_choices,
+        "state_choices": state_choices,
+        "listings": query_list,
+        "values": request.GET,
+    }
+    return render(request, "listings/search.html", context)
