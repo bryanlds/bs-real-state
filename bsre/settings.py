e540b559ba9427aa239c3d39701a805c3df8d651
bryanlds <bryanlds5@gmail.com>
2019-12-30 20:27:33 -0300

    fix contacts 'instaled apps' missing + migrations

--- bsre/settings.py
+++ bsre/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     "src.apps.listings.apps.ListingsConfig",
     "src.apps.realtors.apps.RealtorsConfig",
     "src.apps.accounts.apps.AccountsConfig",
+    "src.apps.contacts.apps.ContactsConfig",
 ]
 
 MIDDLEWARE = [
